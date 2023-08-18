d2ae743afd07431e7f1beb9c0362fc048ffebaf6
Huzaifa Irfan <119890303+Huzaifa1509@users.noreply.github.com>
2023-08-19 03:00:08 +0500

    database
    cvbc

--- visionwebsite.sql
+++ visionwebsite.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 17, 2023 at 09:20 PM
+-- Generation Time: Aug 18, 2023 at 11:59 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -56,7 +56,9 @@ CREATE TABLE `tbl_brand` (
 --
 
 INSERT INTO `tbl_brand` (`brand_id`, `brand_name`) VALUES
-(1, 'Ryzen');
+(1, 'Ryzen'),
+(2, 'Asus'),
+(3, 'Other');
 
 -- --------------------------------------------------------
 
@@ -103,10 +105,26 @@ CREATE TABLE `tbl_products` (
 --
 
 INSERT INTO `tbl_products` (`p_id`, `p_name`, `p_brand`, `p_price`, `p_img`, `p_description`, `p_cat`, `p_qty`) VALUES
-(4, 'Ryzen AM4 B550 ATX Motherboard', 1, 90000, 'motherboard123.jpg', 'yfffvjfjmyhfuuyjhgfj', 2, 50),
-(5, 'Ryzen Ram', 1, 5000, 'ram.jpg', 'ramfdfdfdsfdfsdaffadf', 1, 500),
-(8, 'mboard', 1, 45, 'gpu56.jpg', 'rtrerw', 2, 55),
-(12, 'Ryzen RAM 32GB', 1, 600, 'ram.jpg2023-08-17-18-46', 'gersge', 1, 45);
+(15, 'MSI B550M PRO-VDH WIFI.', 1, 14999, 'm1.jpg2023-08-18-23-55', 'Before purchasing, make sure to check the specific specifications and features of the MSI B550M PRO-VDH WIFI, as motherboard models can vary slightly. This motherboard is well-suited for users who want a compact solution with WiFi capabilities and modern features for their AMD Ryzen system.', 2, 60),
+(16, 'Biostar B450M Motherboard', 1, 16999, 'm2.jpg2023-08-18-23-29', 'The Biostar B450M Motherboard is ideal for users who want a cost-effective solution that covers the basics while providing room for upgrades in the future. It can be a suitable option for building a reliable system for everyday tasks, light gaming, and general computing needs.', 2, 50),
+(17, 'ASUS Z97-P Motherboard', 2, 11999, 'm3.jpg2023-08-18-23-38', 'The ASUS Z97-P motherboard is a solid choice for users seeking a balance between performance and affordability. Built on the Intel Z97 chipset, this ATX motherboard offers a range of features that cater to both casual computing and light gaming needs', 2, 50),
+(18, 'ASUS B560M-Plus Motherboard', 2, 13999, 'm4.jpg2023-08-18-23-09', 'The ASUS B560M-Plus motherboard offers a powerful and feature-rich solution for users seeking performance, versatility, and a smaller form factor. Designed for 10th and 11th generation Intel Core processors, this micro ATX motherboard combines efficiency with modern features.', 2, 50),
+(19, 'G.SKILL Trident Z Memory', 1, 5600, 'r1.jpg2023-08-18-23-22', 'G.SKILL Trident Z memory is the perfect solution for enthusiasts who demand both exceptional performance and aesthetic appeal in their PC builds.', 1, 78),
+(20, 'G.SKILL Trident Z RGB Memory', 1, 4999, 'r2.jpg2023-08-18-23-38', 'G.SKILL Trident Z RGB memory sets a new standard for performance, aesthetics, and customizable lighting effects. Designed for enthusiasts and gamers who crave both high-speed computing and stunning visual impact, Trident Z RGB takes your system to the next level', 1, 90),
+(21, 'ROG Strix RAM', 2, 6999, 'r3.png2023-08-18-23-33', 'ROG Strix RAM is a premium line of memory modules designed by ASUS under their Republic of Gamers (ROG) brand. Engineered for gamers, enthusiasts, and system builders seeking the perfect blend of performance and aesthetics, ROG Strix RAM sets a new standard for memory excellence.', 1, 50),
+(22, 'ROG Certified RAM', 2, 7800, 'r4.jpg2023-08-18-23-40', 'ROG Certified RAM is a hallmark of exceptional memory modules developed by ASUS under their Republic of Gamers (ROG) brand. Created for gamers, enthusiasts, and high-performance seekers, ROG Certified RAM delivers unparalleled performance, reliability, and compatibility for an unmatched gaming experience.', 1, 89),
+(24, 'RX 7900 RADEON', 1, 7800, 'g2.jpg2023-08-18-23-27', ' the \"RX 7900 Radeon\" is indeed a new graphics card model that has been released after September 2021', 3, 90),
+(25, 'ASUS 4070Ti', 2, 12000, 'g2.jpg2023-08-18-23-21', '\"ASUS 4070Ti\" is indeed a new graphics card model that has been released after September 2021', 3, 78),
+(26, 'ROG Strix Gaming', 2, 15000, 'g4.jpg2023-08-18-23-12', '\"ROG Strix Gaming\" is indeed a new graphics card model that has been released after September 2021', 3, 56),
+(27, 'Ryzen Processor', 1, 60000, 'p1.jpg2023-08-18-23-14', 'Ryzen AMD is indeed a new processor that has been released after September 2021', 4, 89),
+(28, 'Ryzen AMD Gaming Processor', 1, 50000, 'p2.jpg2023-08-18-23-32', 'Ryzen AMD is indeed a new gaming processor card model that has been released after September 2021', 4, 678),
+(29, 'Asus Processor', 2, 40000, 'p3.jpg2023-08-18-23-18', '\"ASUS 4070Ti\" is indeed a new processor that has been released after September 2021', 4, 123),
+(30, 'Asus Gaming Processor', 2, 5900, 'p4.jpg2023-08-18-23-15', '\"ASUS Gaming Processor\" is indeed a new graphics card model that has been released after September 2021', 4, 12),
+(31, 'Computer Mouse', 3, 1200, 'o1.jpg2023-08-18-23-36', 'TheMouse is designed to redefine your computing experience, putting precision and comfort at your fingertips. With its ergonomic design, advanced tracking technology, and customizable features, this mouse empowers you to navigate, create, and game with confidence.', 6, 45),
+(32, 'LogiTech Mouse', 3, 2000, 'o2.jpg2023-08-18-23-37', 'The LogiTech mouse is designed to redefine your computing experience, putting precision and comfort at your fingertips. With its ergonomic design, advanced tracking technology, and customizable features, this mouse empowers you to navigate, create, and game with confidence.', 6, 56),
+(33, 'Gaming Keyboard', 3, 900, 'o3.jpg2023-08-18-23-07', 'Introducing the Gaming Keyboard, a keyboard that combines performance, comfort, and style to transform the way you interact with your digital world. With its responsive keys, customizable features, and sleek design, this keyboard is your gateway to enhanced productivity and seamless typing', 6, 12),
+(34, 'Profeesional use Keyboard', 3, 1500, 'o4.jpg2023-08-18-23-57', 'The keyboard that combines performance, comfort, and style to transform the way you interact with your digital world. With its responsive keys, customizable features, and sleek design, this keyboard is your gateway to enhanced productivity and seamless typing', 6, 56),
+(35, 'RX RADEON 600', 1, 5600, 'g1.jpg2023-08-18-23-17', 'AMD today announced the Radeon RX RX 6000 series of gaming graphics cards. Built on the new 7nm RDNA 2 architecture, these cards provide up to 2x improvement in performance', 3, 56);
 
 -- --------------------------------------------------------
 
@@ -128,7 +146,7 @@ CREATE TABLE `tbl_user` (
 --
 
 INSERT INTO `tbl_user` (`id`, `uname`, `lastname`, `email`, `password`, `user_pic`) VALUES
-(1, 'Huzaifa', 'Irfan', 'huzaifairfan2144@gmail.com', '123', 'card.jpg2023-08-17-21-21'),
+(1, 'Huzaifa', 'Irfan', 'huzaifairfan2144@gmail.com', '123', 'card.jpg2023-08-18-23-31'),
 (2, 'ali', 'khan', 'ahmad@gmail.com', '5231', 'images/icons/icon-header-03.png'),
 (3, 'Sarim', 'Khan', 'sarimkhan@gmail.com', '1234', 'images/icons/icon-header-03.png');
 
@@ -211,7 +229,7 @@ ALTER TABLE `tbl_admin`
 -- AUTO_INCREMENT for table `tbl_brand`
 --
 ALTER TABLE `tbl_brand`
-  MODIFY `brand_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `brand_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `tbl_category`
@@ -223,7 +241,7 @@ ALTER TABLE `tbl_category`
 -- AUTO_INCREMENT for table `tbl_products`
 --
 ALTER TABLE `tbl_products`
-  MODIFY `p_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `p_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
 
 --
 -- AUTO_INCREMENT for table `tbl_user`
